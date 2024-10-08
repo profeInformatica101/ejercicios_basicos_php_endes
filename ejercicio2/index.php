185c773d5e90a3b411affb4b2b6ff2a29978f78c
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-10-08 12:34:38 +0200

    Update index.php

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -1,4 +1,17 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <h1>Ejercicio 2</h1>
+    <form action="control.php" method="get">
+        <label for="distancia">Distancia:</label><input type="number" name="distancia" >
+        <label for="velocidad">Velocidad</label><input type="number" name="velocidad" >
+        <input type="submit" value="Enviar">
+    </form>
 
-echo  "<h1>INDEX.PHP</h1>"
-?>
+</body>
+</html>
